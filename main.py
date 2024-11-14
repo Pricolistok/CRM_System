40ab8f12389d92bac52164d311ebe7ba797ba20d
Dokolin Georgij <dga23u865@student.bmstu.ru>
2024-11-15 02:27:56 +0300

    add new requests

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from datetime import date
 
-# from types_of_users import Client
-from requests_to_db import ADD_CLIENT
+from requests_to_db import ADD_CLIENT, CHECK_EQ_USERNAME, FIND_CLIENT, ALL_CLIENTS
 from settings_for_connect import CONNECTION
 
 app = FastAPI()
@@ -16,7 +15,41 @@ async def all_clients() -> str:
 @app.post('/add_new_clients')
 async def add_user(username: str, name: str, surname: str, email: str, password: str, telephone_number: str):
     cursor = connection.cursor()
+    cursor.execute(CHECK_EQ_USERNAME, username)
+    result = cursor.fetchone()
+    print(len(result))
+    if result[0] != 0:
+        return 101
     cursor.execute(ADD_CLIENT, (username, password, name, surname, email, telephone_number, date.today()))
     connection.commit()
+    return 0
 
+@app.get("/find_client_with_username")
+async def find_client(username: str):
+    cursor = connection.cursor()
+    cursor.execute(FIND_CLIENT, username)
+    result = dict()
+    saver = dict()
+    result_from_db = cursor.fetchone()
+    print(result_from_db)
+    arr_names = ["id", "username", "password", "name", "surname", "email", "telephone_number", "date"]
+    for i in range(len(arr_names)):
+        saver[arr_names[i]] = result_from_db[i]
+    result[0] = saver
+    return result
 
+@app.get("/return_all_clients")
+async def find_client() -> dict:
+    cursor = connection.cursor()
+    arr_names = ["id", "username", "password", "name", "surname", "email", "telephone_number", "date"]
+    result = dict()
+    saver = dict()
+    cursor.execute(ALL_CLIENTS)
+    result_from_db = cursor.fetchall()
+    for i in range(len(result_from_db)):
+        for j in range(len(arr_names)):
+            saver[arr_names[j]] = result_from_db[i][j]
+        result[i] = saver
+        saver = dict()
+    print(result)
+    return result
--- requests_to_db.py
+++ requests_to_db.py
@@ -1,4 +1,14 @@
 ADD_CLIENT = '''
     INSERT INTO clients (username, password, name, surname, email, telephone_number, date_of_birth)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    VALUES (%s, %s, %s, %s, %s, %s, %s);
+'''
+FIND_CLIENT = '''
+    SELECT * FROM  clients WHERE username=%s;
+'''
+CHECK_EQ_USERNAME = '''
+    SELECT COUNT(*) FROM clients WHERE username=%s;
+'''
+
+ALL_CLIENTS = '''
+    SELECT * FROM  clients;
 '''
