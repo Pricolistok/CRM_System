bea7331e6d0b198203b8c605e822c2b535e96a8c
Pricolistok <dokolin05@gmail.com>
2024-12-31 02:32:14 +0300

    add close request

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from typing import Annotated
 from fastapi import FastAPI, HTTPException, Response
 from fastapi.params import Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from clients.register_new_client import add_client
 from clients.funcs_for_clients import all_clients, find_client
 from clients.auth_client import auth_client
@@ -17,6 +17,8 @@ app = FastAPI()
 async def root():
     return 'OK'
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 # Ручка для регистрации нового пользователя
 @app.post("/register_new_client")
 async def create_new_client(username: str, password: str, name: str, surname: str, email: str, telephone_number: str):
@@ -26,19 +28,18 @@ async def create_new_client(username: str, password: str, name: str, surname: st
     raise HTTPException(status_code=200, detail="Client registered successfully")
 
 # Функция для авторизации пользователя на площадке
-@app.post("/login_client")
+@app.post("/token")
 async def login_client(response: Response, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     access_token =  auth_client(form_data).access_token
     response.set_cookie(key="users_access_token", value=access_token,  httponly=True)
     return "The Client was logged"
 
-
-# Функция для поиска пользователя в БД
-@app.get("/find_client_with_username")
-async def find_client(username: str):
-    return find_client(username=username)
+# # Функция для поиска пользователя в БД
+# @app.get("/find_client_with_username")
+# async def find_client(username: str):
+#     return find_client(username=username)
 
 # Функция для вывода данных о всех клиентах из БД
 @app.get("/return_all_clients")
-async def find_client() -> dict:
+async def find_client(token: str = Depends(oauth2_scheme)) -> dict:
     return all_clients()
